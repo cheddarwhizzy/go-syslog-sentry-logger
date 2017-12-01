d1ff5e616d649b4021fbdf9e2b9e581756f2e87c
Brett Porter <brett.s.porter@gmail.com>
2017-12-01 07:31:19 -0500

    initial commit

---
+++ syslog.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/getsentry/raven-go"
+	"gopkg.in/mcuadros/go-syslog.v2"
+)
+
+func main() {
+	channel := make(syslog.LogPartsChannel)
+	handler := syslog.NewChannelHandler(channel)
+
+	raven.SetDSN(os.Getenv("SENTRY_DSN"))
+	server := syslog.NewServer()
+	server.SetFormat(syslog.RFC5424)
+	server.SetHandler(handler)
+	server.ListenTCP("0.0.0.0:10514")
+
+	server.Boot()
+
+	go func(channel syslog.LogPartsChannel) {
+		for logParts := range channel {
+			// fmt.Println(logParts)
+			if str, ok := logParts["message"].(string); ok {
+				raven.CaptureMessage(str, nil, nil)
+			}
+		}
+	}(channel)
+
+	server.Wait()
+}
